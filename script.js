8471611160f1273efb2198f151a1c3db59b04903
isaacdharavath94 <281717778+isaacdharavath94@users.noreply.github.com>
2026-08-15 02:42:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
-function showMessage() {
-    document.getElementById("message").innerHTML =
-        "❤️ I Love You Forever ❤️";
+function showLove(){
+
+document.getElementById("loveMessage").innerHTML=
+
+"❤️ You are my favorite notification, my safest place and my happiest memory ❤️";
+
 }
 
-const startDate = new Date("2025-01-01");
+const startDate =
+new Date("2025-01-01");
 
-function updateTimer() {
+function updateCounter(){
 
-    const now = new Date();
+const now = new Date();
 
-    const diff = now - startDate;
+const diff = now - startDate;
 
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+const days =
+Math.floor(
+diff/(1000*60*60*24)
+);
 
-    document.getElementById("timer").innerHTML =
-        "❤️ Together for " + days + " Days ❤️";
+document.getElementById("timer").innerHTML=
+"❤️ Together For " + days + " Days ❤️";
+
 }
 
-updateTimer();
+updateCounter();
 
-setInterval(updateTimer, 1000);
+setInterval(updateCounter,1000);
+
+
+
+function createHeart(){
+
+const heart =
+document.createElement("div");
+
+heart.classList.add("heart");
+
+heart.innerHTML="❤️";
+
+heart.style.left=
+Math.random()*100+"vw";
+
+heart.style.fontSize=
+Math.random()*25+15+"px";
+
+heart.style.animationDuration=
+Math.random()*5+5+"s";
+
+document.querySelector(".hearts")
+.appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},10000);
+
+}
+
+setInterval(createHeart,500);
