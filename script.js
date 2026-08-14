eff48325e876cbc7d288955b3824d024b053e48b
isaacdharavath94 <281717778+isaacdharavath94@users.noreply.github.com>
2026-08-15 02:40:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// Laasya Birthday Website
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "❤️ I Love You Forever ❤️";
+}
+
+const startDate = new Date("2025-01-01");
+
+function updateTimer() {
+
+    const now = new Date();
+
+    const diff = now - startDate;
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+    document.getElementById("timer").innerHTML =
+        "❤️ Together for " + days + " Days ❤️";
+}
+
+updateTimer();
+
+setInterval(updateTimer, 1000);
